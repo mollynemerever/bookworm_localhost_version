bb935c3368c8afca149e48b447df587e5bd79eb9
github username <github email address>
2019-04-15 11:45:19 -0700

    updated staticpages route

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::BooksController < ActionController::Base
+
+# def index
+#   #byebug
+#   render file: 'app/views/books/index.html'
+#   # render :json => Book.all
+#
+#
+# end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
-
-    namespace :api do
-      namespace :v1 do
-        resources :notes, only: [:index, :update]
-      end
+  namespace :api do
+    namespace :v1 do
+      resources :books
+      resources :users
+      resources :staticpages
     end
+  end
 
-
-  
 end
